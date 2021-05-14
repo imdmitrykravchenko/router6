5193b812ff46236443334d89409591cfaf23cb63
dimasik <dimasik@wisebits.com>
2021-05-14 22:00:18 +0300

    export query

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { isRoutingError } from './errors';
-import { RouteDefinition, Route, RouteMiddleware, RouteParams } from './types';
+import { RouteDefinition, Route, RouteMiddleware, RouteParams, Query } from './types';
 import Router6 from './Router6';
 
 export default Router6;
 
-export { isRoutingError, RouteDefinition, Route, RouteMiddleware, RouteParams };
+export { isRoutingError, RouteDefinition, Route, RouteMiddleware, RouteParams, Query };
